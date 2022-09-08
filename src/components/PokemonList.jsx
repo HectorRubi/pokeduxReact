b698ad14bf877e8b6ecf4b212abb7b718124663f
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2022-09-07 19:20:41 -0500

    Integration of PokeAPI using axios

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -2,10 +2,11 @@ import PokemonCard from './PokemonCard';
 import './PokemonList.css';
 
 const PokemonList = ({ pokemons }) => {
+  console.log("file: PokemonsList.jsx ~ line 4 ~ PokemonList ~ pokemons", pokemons);
   return (
     <div className='PokemonList'>
       {pokemons.map(pokemon => {
-        return <PokemonCard />
+        return <PokemonCard name={pokemon.name} key={pokemon.name} />
       })}
     </div>
   )
