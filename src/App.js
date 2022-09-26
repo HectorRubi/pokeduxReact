5a8083d4e7b24bbf2d3caab105d2246542534691
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2022-09-25 19:12:41 -0500

    Add loader

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import { Col } from 'antd';
+import { Col, Spin } from 'antd';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
 import logo from './statics/logo.svg';
 import './App.css';
 import { useEffect } from 'react';
 import { getPokemon } from './api';
-import { getPokemonsWithDetails } from './actions';
+import { getPokemonsWithDetails, setLoading } from './actions';
 
 import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
   const pokemons = useSelector(state => state.pokemons);
+  const loading = useSelector(state => state.loading);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchPokemons = async () => {
+      dispatch(setLoading(true));
       const pokemonsRes = await getPokemon();
       dispatch(getPokemonsWithDetails(pokemonsRes));
+      dispatch(setLoading(false));
     };
 
     fetchPokemons();
@@ -30,7 +33,13 @@ function App() {
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList pokemons={pokemons}/>
+      {loading
+        ?  (
+          <Col offset={12}>
+            <Spin spinning size='large' />
+          </Col>
+        )
+        : <PokemonList pokemons={pokemons}/>}
     </div>
   );
 }
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,16 @@
 import { getPokemonDetails } from "../api";
-import { SET_POKEMONS } from "./types";
+import { SET_LOADING, SET_POKEMONS } from "./types";
 
 // Siempre recibe un payload
 // El payload va a ser, en nuestro caso, los nuevos pokemons
 export const setPokemons = (payload) => ({
   type: SET_POKEMONS,   // Aqui usamos el type definido
   payload,              // Y le pasamos el payload
+});
+
+export const setLoading = (payload) => ({
+  type: SET_LOADING,
+  payload,
 });
 
 export const getPokemonsWithDetails = (pokemons = []) => async (dispatch) => {
